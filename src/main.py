2320e0a5cfe9e8e60811b418eb01f0d7653d7dd0
ElliotEriksson <elliot@serait.se>
2022-04-07 19:38:51 +0200

    Added rerun if choice was invalid

--- src/main.py
+++ src/main.py
@@ -14,57 +14,64 @@ def main():
         create_line()
     elif choice == "3":
         create_train()
-        create_line()
+        create_line()    
+    elif choice == "4":
+        """
+        Lets you pick a train and line then calculates and prints out the answers for you.
+        """
+        print("Pick a train and a line to calculate the time. ")
+        print("-----[THE TRAINS]-----")
+        get_trains()
+        choice_train = int(input("Train: ")) - 1
+        with open("trains.txt", "r", encoding="utf-8") as f:
+            for i, line in enumerate(f):
+                if i == choice_train:
+                    print(line)
+                    stats = line.split("/")
+                    train = Train(stats [0], stats [1], stats [2], stats [3])
+        # Assigning values to the different units
+        a = int(train.get_acceleration())
+        v = int(train.get_speed())
+        r = int(train.get_retardation())
+        s0 = int((v**2)/(2*a))
+        s1 = int((v**2)/(2*r))
 
 
-    print("Pick a train and a line to calculate the time. ")
-    print("-----[THE TRAINS]-----")
-    get_trains()
-    choice_train = int(input("Train: ")) - 1
-    with open("trains.txt", "r", encoding="utf-8") as f:
-        for i, line in enumerate(f):
-            if i == choice_train:
-                print(line)
-                stats = line.split("/")
-                train = Train(stats [0], stats [1], stats [2], stats [3])
+        print("-----[THE LINES]-----")
+        get_lines()
+        choice_line = int(input("Line: ")) - 1
+        with open("lines.txt", "r", encoding="utf-8") as f:
+            for i, line in enumerate(f):
+                if i == choice_line:
+                    print(line)
+                    stats = line.split(",")
+
+        """
+        Calculating the time and printing it out.
+        """
+        x = 1
+        total_time = 0
+        name = stats[0]
+        print(eval(name[1:]))
+        for i in range(int((len(stats)-2)/2)):
+            station = eval(stats[x])
+            x += 1
+            s = int(eval(stats[x]))
+            x += 1
+            if s < s0:
+                time = sqrt(2 * s(1/a + 1/r))
+            elif s > s0:
+                time = v * (1/a + 1/r) + (s - s0 - s1)/v
+            total_time += time
+            print(f"{station}")
+            print(f"{round(time)} seconds")
+        end_station = stats[x]
+        print(eval(end_station[:-1]))
+        print(f"It takes a total of {round(total_time)} seconds to travel across {eval(name[1:])}")
+    else:
+        print(f"{choice} is not an answer. Try again")
+        main()
     
-    a = int(train.get_acceleration())
-    v = int(train.get_speed())
-    r = int(train.get_retardation())
-    s0 = int((v**2)/(2*a))
-    s1 = int((v**2)/(2*r))
-
-    print("-----[THE LINES]-----")
-    get_lines()
-    choice_line = int(input("Line: ")) - 1
-    with open("lines.txt", "r", encoding="utf-8") as f:
-        for i, line in enumerate(f):
-            if i == choice_line:
-                print(line)
-                stats = line.split(",")
-
-    x = 1
-    name = stats[0]
-    print(eval(name[1:]))
-    for i in range(int((len(stats)-2)/2)):
-        station = eval(stats[x])
-        x += 1
-        s = int(eval(stats[x]))
-        x += 1
-        if s < s0:
-            time = sqrt(2 * s(1/a + 1/r))
-        elif s > s0:
-            time = v * (1/a + 1/r) + (s - s0 - s1)/v
-        print(f"{station}")
-        print(f"{round(time)} sekunder")
-    end_station = stats[x]
-    print(eval(end_station[:-1]))
-
-
-
-    print(f"Acc = {a}m/s2 \nSpeed = {v}m/s \nRet = {r}m/s2 \nAcc Dist = {s0}m \nStop Dist = {s1}m")
-
-
-
+    
 if __name__ == "__main__":
     main()
